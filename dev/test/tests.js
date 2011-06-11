9d61d0c8e42b81348fca0b6636f45738ee8720ac
Rozario Chivers <rozario.chivers@lbi.com>
2011-06-11 06:57:53 +0100

    updated pg-home UI tests

--- dev/test/tests.js
+++ dev/test/tests.js
@@ -38,38 +38,39 @@
       ------------------------------------------ */
   if (pageName == "pg-home") { // PAGE NAME CHECK
 
-  module("Layouts");
+      module("Layouts");
 
-  test("Page and Layout UI tests", function() {
-    equals("pg-home", "pg-home", "page name (pg-home) present");
-    equals($("#wrapper").attr("class"), "layout-1-column", "layout: layout-1-column present");
-  });
+      test("Page and Layout UI tests", function() {
+        equals("pg-home", "pg-home", "page name (pg-home) present");
+        equals($("#wrapper").attr("class"), "layout-1-column", "layout: layout-1-column present");
+      });
 
-  module("Components");
+      module("Components");
 
-  test("Component UI tests", function() {
-     ok($("*[data-component-name='skip-links']")[0],"Component: skip-links present");
-     ok($("*[data-component-name='homepage-layout-module']")[0],"Component: homepage-layout-module (1) present");
-     ok($("*[data-component-name='homepage-layout-module']")[1],"Component: homepage-layout-module (2) present");
-     ok($("*[data-component-name='promo']")[0],"Component: promo (1) present");
-     ok($("*[data-component-name='promo']")[1],"Component: promo (2) present");
-     ok($("*[data-component-name='promo']")[2],"Component: promo (3) present");
-     ok($("*[data-component-name='promo']")[3],"Component: promo (4) present");
-     ok($("*[data-component-name='promo']")[4],"Component: promo (5) present");
-     ok($("*[data-component-name='promo']")[5],"Component: promo (6) present");
-     ok($("*[data-component-name='promo']")[6],"Component: promo (7) present");
-     ok($("*[data-component-name='promo']")[7],"Component: promo (8) present");
-     ok($("*[data-component-name='promo']")[8],"Component: promo (9) present");
-     ok($("*[data-component-name='promo']")[9],"Component: promo (10) present");
-     ok($("*[data-component-name='promo']")[10],"Component: promo (11) present");
-     ok($("*[data-component-name='promo']")[11],"Component: promo (12) present");
-     ok($("*[data-component-name='promo']")[12],"Component: promo (13) present");
-     ok($("*[data-component-name='promo']")[13],"Component: promo (14) present");
-     ok($("*[data-component-name='promo']")[14],"Component: promo (15) present");
-     ok($("*[data-component-name='promo']")[15],"Component: promo (16) present");
-     ok($("*[data-component-name='header']")[0],"Component: header present");
-     ok($("*[data-component-name='footer']")[0],"Component: footer present");
-  });
+      test("Component UI tests", function() {
+         ok($("*[data-component-name='skip-links']")[0],"Component: skip-links present");
+         ok($("*[data-component-name='homepage-layout-module']")[0],"Component: homepage-layout-module (1) present");
+         ok($("*[data-component-name='homepage-layout-module']")[1],"Component: homepage-layout-module (2) present");
+         ok($("*[data-component-name='promo']")[0],"Component: promo (1) present");
+         ok($("*[data-component-name='promo']")[1],"Component: promo (2) present");
+         ok($("*[data-component-name='promo']")[2],"Component: promo (3) present");
+         ok($("*[data-component-name='promo']")[3],"Component: promo (4) present");
+         ok($("*[data-component-name='promo']")[4],"Component: promo (5) present");
+         ok($("*[data-component-name='promo']")[5],"Component: promo (6) present");
+         ok($("*[data-component-name='promo']")[6],"Component: promo (7) present");
+         ok($("*[data-component-name='promo']")[7],"Component: promo (8) present");
+         ok($("*[data-component-name='promo']")[8],"Component: promo (9) present");
+         ok($("*[data-component-name='promo']")[9],"Component: promo (10) present");
+         ok($("*[data-component-name='promo']")[10],"Component: promo (11) present");
+         ok($("*[data-component-name='promo']")[11],"Component: promo (12) present");
+         ok($("*[data-component-name='promo']")[12],"Component: promo (13) present");
+         ok($("*[data-component-name='promo']")[13],"Component: promo (14) present");
+         ok($("*[data-component-name='promo']")[14],"Component: promo (15) present");
+         ok($("*[data-component-name='promo']")[15],"Component: promo (16) present");
+         ok($("*[data-component-name='header']")[0],"Component: header present");
+         ok($("*[data-component-name='search']")[0],"Component: search present");
+         ok($("*[data-component-name='footer']")[0],"Component: footer present");
+      });
 
   } // end if (pageName == "pg-home")
 
