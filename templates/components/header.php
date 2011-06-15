21f8940856ece62745ec9c107cf36d694f2a4420
Rozario Chivers <rozario.chivers@lbi.com>
2011-06-15 23:06:04 +0100

    updated components

--- templates/components/header.php
+++ templates/components/header.php
@@ -2,7 +2,7 @@
 	
 	<a href="http://www.site.com" id="logo"><img src="img/chassis-logo.png" class="logo" alt="Chassis Logo" /></a>
 	
-	<?php // include("templates/components/search.php"); ?>
+	<?php include("templates/components/search.php"); ?>
 
     <?php // include("templates/components/header-links.php"); ?>
     
